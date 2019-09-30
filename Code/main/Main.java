8d87322bf0ad6da131199d61eb5b08d7964fd30b
Félix Romero José Francisco <51935664+p4n5ho@users.noreply.github.com>
2019-09-30 09:56:19 -0700

    Add files via upload

---
+++ Code/main/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import Presentacion.frmInicioSesion;
+
+/**
+ *
+ * @author Alejandro Galindo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        frmInicioSesion inicio = new frmInicioSesion();
+        inicio.setVisible(true);
+    }
+    
+}
